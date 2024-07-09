6cc307816207b3223015ed084ed7c8f07ad3b72f
RLSKM <158546198+RLSKM@users.noreply.github.com>
2024-07-09 17:03:49 +0800

    Update Basic.js

--- Basic.js
+++ Basic.js
@@ -1,1 +1,25 @@
+let variable = 123;  //create variable
+
+//creates a constant(cannot be changed)
+const birthday = "12-09-07";
+let isHuman = true; //Capital not required
+
+console.log(variable);  //print
+
+let a = Math.random();  //No need to import
+let b = Math.round(12.438);
+let c = Math.E ** 2;
+
+//Strings
+let firstName = 'Andrew';
+let nameLength = firstName.length;
+
+let language = 'english';
+console.log(language[0]);
+console.log(language.substring(2, 5));  //from index 2 to 4
+console.log(language.toUpperCase());
+console.log(language.replace('eng', 'eye'));
+//if includes java, return true
+console.log(language.includes('Java'));
+console.log(`I hate ${language.toLowerCase()}`)  //f string
 
