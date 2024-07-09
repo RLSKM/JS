ab307a9f75688983f04f53b19f48367ffddd0a04
RLSKM <158546198+RLSKM@users.noreply.github.com>
2024-07-09 17:30:04 +0800

    Update Basic.js

--- Basic.js
+++ Basic.js
@@ -22,4 +22,59 @@ console.log(language.replace('eng', 'eye'));
 //if includes java, return true
 console.log(language.includes('Java'));
 console.log(`I hate ${language.toLowerCase()}`)  //f string
+// logic
+let age = prompt("enter age");
+
+if (age <= 12) {
+    console.log("L kid");
+}
+else if (age <= 20) {
+    console.log("L adult");
+}
+
+// loops
+let fruits = ['apple', 'banana', 'orange', 'coconut'];
+
+for (let i = 0); i < fruit.length; i++){
+    console.log(fruits[i]);
+}
+for (let fruit of fruits){
+    console.log(fruit;)
+}
+
+let counter = 30
+while (counter > 0){
+    console.log(counter);
+    counter -- ;
+}
+console.log("U died")
+
+// Arrays (lists)
+let numArr = [1, 2, 3, 4, 5];
+let schArr = ['ASR', 'EJC', 'AHS'];
+
+console.log(numArr[0]);
+schArr.push('TMJC');  // Appends to end of array
+schArr.pop();  // Removes last element in array
+
+// Objects (Dictionaries)
+
+let person = {
+    firstName: 'Andrew',
+    lastName: 'Me',
+    age: 12
+};
+
+person['age'] = 21;
+console.log(person.age);  //either is fine
+console.log(person['firstName']);
+
+//Functions
+
+function cube(x) {
+    return x ** 3;
+}
+
+// Arrow function (short functions)
+let magnitude = (x, y, z) => Math.sqrt(x**2 + y**2 + z**2);
 
